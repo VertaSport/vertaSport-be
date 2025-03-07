58979ec96e2a995d312d1dbc9f5c18deba1ae5b4
nguyenthonghuy16012003 <huyten26@gmail.com>
2025-03-07 16:07:02 +0700

    revert: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,31 @@
-// import app from './app';
-// import connectDB from './config/database.config';
-// import config from './config/env.config';
-// import { confirmWebhook } from './services/payos.service';
-
-// const PORT = config.port || 8080;
-// const HOSTNAME = config.hostname;
-
-// let server: any;
-// connectDB().then(async () => {
-//     server = app.listen(PORT, `${HOSTNAME}`, async () => {
-//         const ngrok = await import('ngrok');
-//         const ngrokUrl = await ngrok.connect({ addr: PORT, authtoken: process.env.NGROK_AUTHTOKEN });
-
-//         await confirmWebhook(`${ngrokUrl}/webhook`);
-
-//         console.log(`Listening to port ${PORT}`);
-//         console.log(`Ingress established at: ${ngrokUrl}`);
-//     });
-// });
-
-// const exitHandler = () => {
-//     if (server) {
-//         server.close(async () => {
-//             const ngrok = await import('ngrok');
-//             if (ngrok) {
-//                 await ngrok.kill();
-//             }
-//             console.log('Server closed');
-//             process.exit(1);
-//         });
-//     } else {
-//         process.exit(1);
-//     }
-// };
-
-// const unexpectedErrorHandler = (error: string) => {
-//     console.log(error);
-//     exitHandler();
-// };
-
-// process.on('uncaughtException', unexpectedErrorHandler);
-// process.on('unhandledRejection', unexpectedErrorHandler);
-
-// process.on('SIGTERM', () => {
-//     console.log('SIGTERM received');
-//     if (server) {
-//         server.close();
-//     }
-// });
-
-// ================
-
 import app from './app';
 import connectDB from './config/database.config';
 import config from './config/env.config';
+import { confirmWebhook } from './services/payos.service';
 
 const PORT = config.port || 8080;
 const HOSTNAME = config.hostname;
 
 let server: any;
 connectDB().then(async () => {
-    server = app.listen(PORT, `${HOSTNAME}`, () => {
+    server = app.listen(PORT, `${HOSTNAME}`, async () => {
+        const ngrok = await import('ngrok');
+        const ngrokUrl = await ngrok.connect({ addr: PORT, authtoken: process.env.NGROK_AUTHTOKEN });
+
+        await confirmWebhook(`${ngrokUrl}/webhook`);
+
         console.log(`Listening to port ${PORT}`);
+        console.log(`Ingress established at: ${ngrokUrl}`);
     });
 });
 
 const exitHandler = () => {
     if (server) {
-        server.close(() => {
+        server.close(async () => {
+            const ngrok = await import('ngrok');
+            if (ngrok) {
+                await ngrok.kill();
+            }
             console.log('Server closed');
             process.exit(1);
         });
@@ -90,3 +48,45 @@ process.on('SIGTERM', () => {
         server.close();
     }
 });
+
+// ================
+
+// import app from './app';
+// import connectDB from './config/database.config';
+// import config from './config/env.config';
+
+// const PORT = config.port || 8080;
+// const HOSTNAME = config.hostname;
+
+// let server: any;
+// connectDB().then(async () => {
+//     server = app.listen(PORT, `${HOSTNAME}`, () => {
+//         console.log(`Listening to port ${PORT}`);
+//     });
+// });
+
+// const exitHandler = () => {
+//     if (server) {
+//         server.close(() => {
+//             console.log('Server closed');
+//             process.exit(1);
+//         });
+//     } else {
+//         process.exit(1);
+//     }
+// };
+
+// const unexpectedErrorHandler = (error: string) => {
+//     console.log(error);
+//     exitHandler();
+// };
+
+// process.on('uncaughtException', unexpectedErrorHandler);
+// process.on('unhandledRejection', unexpectedErrorHandler);
+
+// process.on('SIGTERM', () => {
+//     console.log('SIGTERM received');
+//     if (server) {
+//         server.close();
+//     }
+// });
